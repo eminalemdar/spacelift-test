e943e1292a3e889b06034894ca44e691169ddf78
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2024-06-05 15:05:30 +0300

    Update stack.tf

--- stack.tf
+++ stack.tf
@@ -1,4 +1,8 @@
 resource "spacelift_stack" "test" {
+  github_enterprise {
+    namespace = "eminalemdar"
+    id = "github-enterprise-default-integration"
+  }
   branch            = "main"
   description       = "test"
   name              = "test"
